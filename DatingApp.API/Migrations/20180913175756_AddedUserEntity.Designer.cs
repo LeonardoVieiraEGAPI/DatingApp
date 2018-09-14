3330129f8e861b345c731c51837f2f0057de8207
Leonardo Vieira <lvieira@egapi.local>
2018-09-14 11:50:20 +0100

    End of Section 10

--- DatingApp.API/Migrations/20180913175756_AddedUserEntity.Designer.cs
+++ DatingApp.API/Migrations/20180913175756_AddedUserEntity.Designer.cs
@@ -9,8 +9,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20180912150340_ExtendedUserClass")]
-    partial class ExtendedUserClass
+    [Migration("20180913175756_AddedUserEntity")]
+    partial class AddedUserEntity
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -57,7 +57,7 @@ namespace DatingApp.API.Migrations
 
                     b.Property<string>("Interests");
 
-                    b.Property<string>("Intriduction");
+                    b.Property<string>("Introduction");
 
                     b.Property<string>("KnownAs");
 
